5d1c3fe09d6c7b91805897d5cc8bc30b3397d0fb
JoelAnthony1 <joelanthony.2023@smu.edu.sg>
2024-11-11 18:23:25 +0800

    restapitest

--- src/main/java/csd/cheaterbug/CheaterbugController.java
+++ src/main/java/csd/cheaterbug/CheaterbugController.java
@@ -1,27 +1,20 @@
 package csd.cheaterbug;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import csd.cheaterbug.Entity.Request;
 import csd.cheaterbug.Entity.Response;
 import csd.cheaterbug.Entity.Result;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/cheaterbug")
 public class CheaterbugController {
 
     private static final Logger logger = LoggerFactory.getLogger(CheaterbugController.class);
-
     private final CheaterbugService cheaterbugService;
 
     public CheaterbugController(CheaterbugService cheaterbugService) {
@@ -30,18 +23,14 @@ public class CheaterbugController {
 
     @PostMapping("/analysis")
     public Response analyseResults(@RequestBody List<Request> request) {
-        // TODO remove DEBUG 1
-        // logger.info("Received JSON payload: {}", request);
+        List<Result> resultsList = request.stream()
+            .map(r -> {
+                Result result = new Result(r.getExpectedScore(), r.getActualScore());
+                logger.info(result.toString());
+                return result;
+            })
+            .collect(Collectors.toList());
 
-        List<Result> resultsList = new ArrayList<>();
-        for (Request r : request) {
-            // TODO remove DEBUG 2
-            // logger.info(String.format("Received Expected: %f Actual: %f", r.getExpectedScore(), r.getActualScore()));
-            Result result = new Result(r.getExpectedScore(), r.getActualScore());
-            // TODO remove DEBUG 3
-            logger.info(result.toString());
-            resultsList.add(result);
-        }
         return cheaterbugService.getAnalysis(resultsList);
     }
 }
--- src/main/java/csd/cheaterbug/CheaterbugService.java
+++ src/main/java/csd/cheaterbug/CheaterbugService.java
@@ -2,14 +2,10 @@ package csd.cheaterbug;
 
 import java.util.TreeMap;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Service;
-
 import csd.cheaterbug.Entity.ProbabilityObservationMap;
 import csd.cheaterbug.Entity.Response;
 import csd.cheaterbug.Entity.Result;
@@ -20,80 +16,65 @@ public class CheaterbugService {
     private static final Logger logger = LoggerFactory.getLogger(CheaterbugService.class);
 
     /**
-     * Given a list of results, this method calculates the probability of the player achieving this result
+     * Analyzes a list of results and calculates probabilities for expected and cheat scenarios.
      * @param results List of Result objects
-     * @return Response object containing the probabilities of expected and cheat
+     * @return Response object with calculated probabilities
      */
     public Response getAnalysis(List<Result> results) {
-        String expectedType = "expected";
-        ProbabilityObservationMap expectedProbabilityObservations = calculateProbObsMap(results, expectedType);
-        logger.info("Expected Probability Observations: " + expectedProbabilityObservations.toString());
+        ProbabilityObservationMap expectedObservations = calculateProbObsMap(results, "expected");
+        logger.info("Expected Probability Observations: {}", expectedObservations);
 
-        String cheatType = "cheat";
-        ProbabilityObservationMap cheatProbabilityObservations = calculateProbObsMap(results, cheatType);
-        logger.info("Cheat Probability Observations: " + cheatProbabilityObservations.toString());
-        
+        ProbabilityObservationMap cheatObservations = calculateProbObsMap(results, "cheat");
+        logger.info("Cheat Probability Observations: {}", cheatObservations);
+
         return new Response(
-            new TreeMap<>() {{
-                put("actual", Double.toString(getActualProbability(results, expectedType)));
-                put("1stPercentile", expectedProbabilityObservations.getNthPercentileProbability(0.01));
-                put("5thPercentile", expectedProbabilityObservations.getNthPercentileProbability(0.05));
-            }},
-            new TreeMap<>() {{
-                put("actual", Double.toString(getActualProbability(results, cheatType)));
-                put("80thPercentile", cheatProbabilityObservations.getNthPercentileProbability(0.80));
-                put("90thPercentile", cheatProbabilityObservations.getNthPercentileProbability(0.90));
-                put("95thPercentile", cheatProbabilityObservations.getNthPercentileProbability(0.95));
-                put("99thPercentile", cheatProbabilityObservations.getNthPercentileProbability(0.99));
-            }});       
+            createProbabilityMap(results, expectedObservations, "expected", 0.01, 0.05),
+            createProbabilityMap(results, cheatObservations, "cheat", 0.80, 0.90, 0.95, 0.99)
+        );
     }
 
-    private ProbabilityObservationMap calculateProbObsMap(final List<Result> results, String type) {
-        List<Result> resultsCopy = new ArrayList<>(results);
+    private ProbabilityObservationMap calculateProbObsMap(List<Result> results, String type) {
+        ProbabilityObservationMap probabilityObservations = new ProbabilityObservationMap();
+        double initialProbability = 1.0;
+        int initialCount = 1;
 
-        // Use first Result object to initialise probabilityObservations map, and remove from results list
-        Result firstResult = resultsCopy.get(0);
-        resultsCopy.remove(0);
+        Result firstResult = results.get(0);
+        probabilityObservations = addEntries(probabilityObservations, initialProbability, firstResult, initialCount, type);
 
-        Double initialProbability = 1.0;
-        Integer initialCount = 1;
-        ProbabilityObservationMap probabilityObservations = addResultsToProbObsMap(new ProbabilityObservationMap(), initialProbability, firstResult, initialCount, type);
-
-        for (Result result : resultsCopy) {
+        for (Result result : results.subList(1, results.size())) {
             probabilityObservations = updateProbObsMap(probabilityObservations, result, type);
         }
-
         return probabilityObservations;
     }
 
-    private Double getActualProbability(List<Result> results, String type) {
-        Double probability = 1.0;
-        for (Result result : results) {
-            if (result.getActualScore() == 0.0) {
-                probability *= result.getPLoseType(type);
-            } else if (result.getActualScore() == 1.0) {
-                probability *= result.getPWinType(type);
-            } else {
-                probability *= result.getPDrawType(type);
-            }
+    private Map<String, String> createProbabilityMap(List<Result> results, ProbabilityObservationMap observations, String type, double... percentiles) {
+        TreeMap<String, String> map = new TreeMap<>();
+        map.put("actual", Double.toString(getActualProbability(results, type)));
+        for (double percentile : percentiles) {
+            map.put((int)(percentile * 100) + "thPercentile", observations.getNthPercentileProbability(percentile));
         }
-        return probability;
+        return map;
     }
 
-    private ProbabilityObservationMap addResultsToProbObsMap(ProbabilityObservationMap probabilityObservations, Double probability, Result firstResults, Integer count, String type) {
-        probabilityObservations.addEntry(probability * firstResults.getPLoseType(type), count);
-        probabilityObservations.addEntry(probability * firstResults.getPDrawType(type), count);
-        probabilityObservations.addEntry(probability * firstResults.getPWinType(type), count);
-        return probabilityObservations;
+    private double getActualProbability(List<Result> results, String type) {
+        return results.stream()
+                .mapToDouble(result -> result.getActualScore() == 0.0 ? result.getPLoseType(type) :
+                        result.getActualScore() == 1.0 ? result.getPWinType(type) : result.getPDrawType(type))
+                .reduce(1.0, (a, b) -> a * b);
     }
 
-    private ProbabilityObservationMap updateProbObsMap(ProbabilityObservationMap probabilityObservations, Result result, String type) {
+    private ProbabilityObservationMap addEntries(ProbabilityObservationMap probObsMap, double probability, Result result, int count, String type) {
+        probObsMap.addEntry(probability * result.getPLoseType(type), count);
+        probObsMap.addEntry(probability * result.getPDrawType(type), count);
+        probObsMap.addEntry(probability * result.getPWinType(type), count);
+        return probObsMap;
+    }
+
+    private ProbabilityObservationMap updateProbObsMap(ProbabilityObservationMap probObsMap, Result result, String type) {
         ProbabilityObservationMap newProbObsMap = new ProbabilityObservationMap();
-
-        for(Map.Entry<Double, Integer> observation : probabilityObservations.getEntries()) {
-            addResultsToProbObsMap(newProbObsMap, observation.getKey(), result, observation.getValue(), type);
-        }
-
+        probObsMap.getEntries().forEach(observation ->
+                addEntries(newProbObsMap, observation.getKey(), result, observation.getValue(), type)
+        );
         return newProbObsMap;
     }
 }
